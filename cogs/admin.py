f02690467b8dac6ec49c54b77033653794c1cc91
MrOctopus <shock.game@hotmail.com>
2020-06-28 19:52:22 +0200

    Added clear all command. Also moved _animate and made it a static method

--- cogs/admin.py
+++ cogs/admin.py
@@ -3,6 +3,12 @@ import asyncio
 from discord.ext import commands
 
 #
+# CONSTANTS
+#
+
+HISTORY_LIMIT = 300
+
+#
 # CLASSES
 #
 
@@ -14,13 +20,24 @@ class Admin(commands.Cog):
     async def cog_check(self, ctx):
         return ctx.channel.permissions_for(ctx.message.author).administrator
         
-    @commands.command()
-    async def clear(self, ctx, *, lim=30):
-        if lim <= 0 or lim > 300:
-            await ctx.send("Choose a limit between 1 and 300")
+    @commands.group(name = 'clear', invoke_without_command = True, description = "Clears all bot commands and messages in the channel, given a limit parameter. Default is 30.")
+    async def _clear(self, ctx, *, lim=30):
+        if lim <= 0 or lim > HISTORY_LIMIT:
+            await ctx.send("Choose a limit between 1 and {}".format(HISTORY_LIMIT))
         else:
-            isCmdOrBot = lambda msg: True if msg.author == self.client.user or msg.content.startswith(self.client.command_prefix) else False
+            botuser = self.client.user
+            prefix = self.client.command_prefix
+
+            isCmdOrBot = lambda msg: True if msg.author == botuser or msg.content.startswith(prefix) else False
+            
             await ctx.channel.purge(limit=lim, check=isCmdOrBot, before=ctx.message, bulk=True)
+
+    @_clear.command(name = 'all', description = "Clears all chat messages in the channel, given a limit parameter. Default is 30.")
+    async def _clear_all(self, ctx, *, lim=30):
+        if lim <= 0 or lim > HISTORY_LIMIT:
+            await ctx.send("Choose a limit between 1 and {}".format(HISTORY_LIMIT))
+        else:
+            await ctx.channel.purge(limit=lim, before=ctx.message, bulk=True)
 
 #
 # SETUP
--- cogs/animations.py
+++ cogs/animations.py
@@ -3,17 +3,10 @@ import asyncio
 from discord.ext import commands
 
 #
-# HELPER
+# CONSTANTS
 #
 
 UPDATE_TICK = 0.3
-
-async def _animate(ctx, animation, *, num_loop = 1):
-    message = await ctx.send(animation[0])
-    for i in range(num_loop):
-        for frame in animation:
-            await message.edit(content=frame)
-            await asyncio.sleep(UPDATE_TICK)
 
 #
 # CLASSES
@@ -23,15 +16,23 @@ class Animations(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @staticmethod
+    async def _animate(ctx, animation, *, num_loop = 1):
+        message = await ctx.send(animation[0])
+        for i in range(num_loop):
+            for frame in animation:
+                await message.edit(content=frame)
+                await asyncio.sleep(UPDATE_TICK)
+
     @commands.command()
     async def howdy(self, ctx):
         animation = [":thumbsup:          :cowboy:\n   :eggplant::zzz::necktie::eggplant:\n               :oil:    :eggplant:\n               :zap:8==:punch:D:sweat_drops:\n           :carrot:  :eggplant:\n           :boot:     :boot:",":thumbsup:          :cowboy:\n   :eggplant::zzz::necktie::eggplant:\n               :oil:    :nose:\n               :zap:8=:punch:=D:sweat_drops:\n           :carrot:  :eggplant:                  :sweat_drops:\n           :boot:     :boot:",":thumbsup:          :cowboy:\n   :eggplant::zzz::necktie::nose:\n               :oil:  :nose:\n               :zap:8:punch:==D:sweat_drops:\n           :carrot:  :eggplant:                  :sweat_drops:\n           :boot:     :boot:                   :sweat_drops:",":thumbsup:          :cowboy:\n   :eggplant::zzz::necktie::eggplant:\n               :oil:    :nose:\n               :zap:8=:punch:=D:sweat_drops:\n           :carrot:  :eggplant:                  :sweat_drops:\n           :boot:     :boot:"]
-        await _animate(ctx, animation, num_loop = 20)
+        await Animations._animate(ctx, animation, num_loop = 20)
 
     @commands.command()
     async def howdysplurt(self, ctx):
         animation = [":thumbsup:          :cowboy:\n   :eggplant::zzz::necktie::eggplant:\n               :oil:    :eggplant:\n               :zap:8==:punch:D\n           :carrot:  :eggplant:\n           :boot:     :boot:",":thumbsup:          :cowboy:\n   :eggplant::zzz::necktie::eggplant:\n               :oil:    :nose:\n               :zap:8=:punch:=D\n           :carrot:  :eggplant:\n           :boot:     :boot:",":thumbsup:          :cowboy:\n   :eggplant::zzz::necktie::nose:\n               :oil:  :nose:\n               :zap:8:punch:==D\n           :carrot:  :eggplant:\n           :boot:     :boot:",":thumbsup:          :cowboy:\n   :eggplant::zzz::necktie::eggplant:\n               :oil:    :nose:\n               :zap:8=:punch:=D\n           :carrot:  :eggplant:\n           :boot:     :boot:",":thumbsup:          :cowboy:\n   :eggplant::zzz::necktie::eggplant:\n               :oil:    :eggplant:\n               :zap:8==:punch:D\n           :carrot:  :eggplant:\n           :boot:     :boot:",":thumbsup:          :cowboy:\n   :eggplant::zzz::necktie::eggplant:\n               :oil:    :nose:\n               :zap:8=:punch:=D\n           :carrot:  :eggplant:\n           :boot:     :boot:",":thumbsup:          :cowboy:\n   :eggplant::zzz::necktie::nose:\n               :oil:  :nose:\n               :zap:8:punch:==D\n           :carrot:  :eggplant:\n           :boot:     :boot:",":thumbsup:          :cowboy:\n   :eggplant::zzz::necktie::eggplant:\n               :oil:    :nose:\n               :zap:8=:punch:=D:ocean:\n           :carrot:  :eggplant:                  :ocean:\n           :boot:     :boot:                    :ocean:",":thumbsup:          :cowboy:\n   :eggplant::zzz::necktie::eggplant:\n               :oil:    :nose:\n               :zap:8=:punch:=D:ocean::ocean::ocean::ocean::ocean:\n           :carrot:  :eggplant:                  :ocean::ocean::ocean::ocean::ocean:\n           :boot:     :boot:                    :ocean::ocean::ocean::ocean::ocean:",":thumbsup:          :cowboy:\n   :eggplant::zzz::necktie::eggplant:\n               :oil:    :nose:\n               :zap:8=:punch:=D:ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean:\n           :carrot:  :eggplant:                  :ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean:\n           :boot:     :boot:                    :ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean:",":thumbsup:          :cowboy:\n   :eggplant::zzz::necktie::eggplant:\n               :oil:    :nose:\n               :zap:8=:punch:=D:ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean:\n           :carrot:  :eggplant:                  :ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean:\n           :boot:     :boot:                    :ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean:",":thumbsup:          :cowboy:\n   :eggplant::zzz::necktie::eggplant:\n               :oil:    :nose:\n               :zap:8=:punch:=D:ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean:\n           :carrot:  :eggplant:                  :ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean:\n           :boot:     :boot:                    :ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean:",":thumbsup:          :frowning:\n   :eggplant::zzz::necktie::eggplant:\n               :oil:    :nose:\n               :zap:8=:punch:=D:ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean:\n           :carrot:  :eggplant:                  :ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean:\n           :boot:     :boot:                    :ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean::ocean:",":thumbsup:          :frowning:\n   :eggplant::zzz::necktie::eggplant:\n               :oil:    :nose:\n               :zap:8=:punch::boom:=D\n           :carrot:  :eggplant:\n           :boot:     :boot:",":thumbsup:          :frowning:\n   :eggplant::zzz::necktie::eggplant:\n               :oil:    :nose:\n               :zap:8=:punch:           =D\n           :carrot:  :eggplant:\n           :boot:     :boot:",":thumbsup:          :frowning:\n   :eggplant::zzz::necktie::eggplant:\n               :oil:    :nose:\n               :zap:8=:punch:           :red_circle:=D\n           :carrot:  :eggplant:\n           :boot:     :boot:",":thumbsup:          :frowning:\n   :eggplant::zzz::necktie::eggplant:\n               :oil:    :nose:\n               :zap:8=:punch:           :red_circle:\n           :carrot:  :eggplant:                    :red_circle:=D\n           :boot:     :boot:",":thumbsup:          :frowning:\n   :eggplant::zzz::necktie::eggplant:\n               :oil:    :nose:\n               :zap:8=:punch:           :red_circle:\n           :carrot:  :eggplant:                    :red_circle:\n           :boot:     :boot:                   :red_circle:=D",":thumbsdown:          :sob:\n   :eggplant::zzz::necktie::eggplant:\n               :oil:    :nose:\n               :zap:8=:punch::red_circle:\n           :carrot:  :eggplant:         :red_circle:\n           :boot:     :boot:       :red_circle::red_circle::red_circle:=D"]
-        await _animate(ctx, animation)
+        await Animations._animate(ctx, animation)
 
 #
 # SETUP

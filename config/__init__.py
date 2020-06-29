e2a331269677972be8eb1f3d4fc8c9fe841787c0
MrOctopus <shock.game@hotmail.com>
2020-06-29 23:31:09 +0200

    Changed default example

--- config/__init__.py
+++ config/__init__.py
@@ -18,7 +18,7 @@ def get():
         if e.errno == 2:
             logging.warning("The config.json file is missing, creating a new one")
 
-            exampleConf = {'discordToken':'fdjkakjdfefehsabh93,.3mejnfe', 'commandPrefix':'?', 'ownerIds':[] , 'ytdlFormatOptions': {'format': 'bestaudio/best','outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s', 'restrictfilenames': True, 'noplaylist': True, 'nocheckcertificate': True, 'ignoreerrors': False, 'logtostderr': False, 'quiet': True, 'no_warnings': True, 'default_search': 'auto', 'source_address': '0.0.0.0',"HighWaterMark":3145728}, 'ffmpeg_options':{'options': '-vn'},"ffmpeg_before_options":{"reconnect": 1, "reconnect_streamed": 1, "reconnect_delay_max":5}}
+            exampleConf = {'discordToken':'fdjkakjdfefehsabh93,.3mejnfe', 'commandPrefix':'?', 'ownerIds':[] , 'ytdlFormatOptions': {'format': 'bestaudio/best','outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s', 'restrictfilenames': True, 'noplaylist': True, 'nocheckcertificate': True, 'ignoreerrors': False, 'logtostderr': False, 'quiet': True, 'no_warnings': True, 'default_search': 'auto', 'source_address': '0.0.0.0',"HighWaterMark":3145728}, "ffmpeg_options":"-vn","ffmpeg_before_options":"-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"}
             saveJson(exampleConf, "./config/config.json")
 
             logging.critical("Fill the required fields in the config.json file and run the script again")

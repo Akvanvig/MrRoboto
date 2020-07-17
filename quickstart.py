eae26a24a1c7227f38e1219c83e982214d8c0689
MrOctopus <shock.game@hotmail.com>
2020-07-17 20:45:45 +0200

    Finished quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -6,6 +6,8 @@ import platform
 import json
 import subprocess
 
+# TODO(Fredrico): Add error messages to ReqError and CompatError exceptions
+
 #
 # CONSTANTS
 #
@@ -65,14 +67,16 @@ class ReqError(Exception):
 
 def input_yn(string : str) -> bool:
     while True:
-        answer = input(string + " (y/n):    ").lower()
+        answer = input(string + " [y/n]:    ").lower()
         if answer in ('y', 'yes'): return True
         elif answer in ('n', 'no'): return False
         else: print("Error, enter a valid input")
 
 def install_requirements_pip():
     try:
+        print("...Attempting to install pip requirements")
         subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", os.path.join(FILE_DIR, "bot/requirements.txt")])
+        print("...Success, installed pip requirements")
     except subprocess.CalledProcessError:
         raise ReqError()
 
@@ -88,15 +92,18 @@ def install_requirements_windows():
     # Download and install ffmpeg
 
     try:
+        print("...Attempting to download ffmpeg")
         with urlopen(Request(
             url = "https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.zip", 
             headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'})
         ) as urlObj:
             url_file = BytesIO(urlObj.read())
+        print("...Success, downloaded ffmpeg")
     except (URLError, IncompleteRead):
         raise ReqError()
 
     with ZipFile(url_file, mode = 'r') as zipObj:
+        print("...Extracting ffmpeg from archive")
         for zip_info in zipObj.infolist():
             if zip_info.filename[-1] == '/': continue
             
@@ -107,57 +114,67 @@ def install_requirements_windows():
             zip_info.filename = os.path.join("bot", basename)
             zipObj.extract(zip_info)
             break
-
-    # Download and install pip requirements
-
-    install_requirements_pip()
+        print("...Success, extracted ffmpeg.exe from archive")
     
 def install_requirements_linux():
     # For now let's assume the distro is debian
     # or else we have to install another pip package to detect the distro
 
     # Download and install apt packages
-
     try:
-        pass
+        print("...Attempting to download dev dependencies")
+        subprocess.check_call(['sudo', 'apt', 'install', 'libffi-dev', 'libnacl-dev', 'libpq-dev', '-y'], stdout=sys.stdout, stderr=sys.stderr)
+        subprocess.check_call(['sudo', 'apt', 'install', 'python3-pip', '-y'], stdout=sys.stdout, stderr=sys.stderr)
+        print("...Success, dev dependencies downloaded")
     except subprocess.CalledProcessError:
         raise ReqError()
-
-    # Download and install pip requirements
-
-    install_requirements_pip()
 
 def gen_configs(path : str):    
     if not os.path.exists(path):
         os.makedirs(path)
 
+    print("...Attempting to generate secrets.json file in {}.".format(path))
     file = open(os.path.join(path, "secrets.json"), 'w')
     json.dump(EXAMPLE_SECRETS, file, indent=4)
     file.close()
+    print("...Success, file generated")
     
+    print("...Attempting to generate bot.json file in {}.".format(path))
     file = open(os.path.join(path, "bot.json"), 'w')
     json.dump(EXAMPLE_CONFIG, file, indent=4)
     file.close()
+    print("...Success, file generated")
 
-def local(install : bool, generate : bool):
+def local(*, install : bool, generate : bool):
     if install:
         system_name = platform.system()
 
         try:
+            print("\n[INSTALLING REQUIREMENTS]")
+            # System specific install requirements
             if system_name == 'Windows': install_requirements_windows()
             elif system_name == 'Linux': install_requirements_linux()
             else: raise CompatError()
+
+            # Download and install pip requirements
+            install_requirements_pip()
         except CompatError:
-            print("Error, you are not on a supported system")
+            print("...Error, you are not on a supported system")
         except ReqError:
-            print("Error, failed to download a requirement")
+            print("...Error, failed to download a requirement")
+        else:
+            print("[FINISHED INSTALLATION]")
 
     if generate: 
+        print("\n[GENERATING CONFIGS]")
         gen_configs(os.path.join(FILE_DIR, "bot/config/"))
+        print("[FINISHED GENERATING CONFIGS]")
 
-def kubernetes(install : bool, generate : bool):
+def kubernetes(*, install : bool, generate : bool):
     if generate: 
+        print("\n[GENERATING CONFIGS]")
         gen_configs(os.path.join(FILE_DIR,"config/"))
+        print("[FINISHED GENERATING CONFIGS]")
 
 #
 # MAIN
@@ -169,14 +186,21 @@ def main():
     # Ask for type
     # Ask if user wants to install requirements
     # Ask if user wants to generate example configs
-    use_type = input_yn("Type (Y)es if you are using the bot locally, or (N)o if you are using kubernetes")
-    reqs = input_yn("Do you want to install all requirements?")
-    configs = input_yn("Do you want to generate example config files?")
+    use_type = input_yn("Input: \n (Y)es if you are using the bot locally\n (N)o if you are using kubernetes\n")
+    print('')
+
+    if input_yn("Is this your first time running quickstart on this machine?"):
+        reqs = configs = True
+    else:
+        reqs = input_yn("Do you want to install all the requirements?")
+        configs = input_yn("Do you want to generate example config files?")
 
     if use_type:
         local(install = reqs, generate = configs)
     else:
         kubernetes(install = reqs, generate = configs)
+
+    print("\nQuickstart finished running")
 
 if __name__ == '__main__':
     main()

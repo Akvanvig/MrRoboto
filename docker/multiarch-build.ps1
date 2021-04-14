d353ba4517290e6c4acea24eb0cd356b4efb803a
akvanvig <akvanv@gmail.com>
2021-04-14 20:51:09 +0200

    Might be retarteded, should be fixed now 💦💦💦

--- docker/multiarch-build.ps1
+++ docker/multiarch-build.ps1
@@ -9,7 +9,7 @@ $repository = "ghcr.io"
 # Functions
 $buildImage = {
   param($platform, $username, $repository)
-  docker build --no-cache -t "$repository/$username/mrroboto_no-audio:$platform" -f ./docker/bot.dockerfile . --build-arg ARCH=$platform
+  docker build --no-cache -t "$repository/$username/mrroboto:$platform" -f ./docker/bot.dockerfile . --build-arg ARCH=$platform
 }
 
 # Pre-requisite tests
@@ -30,7 +30,8 @@ Foreach ($platform in $platforms){
 
 ## Check if images created
 Do {
-  $out = Get-Job
+  Start-Sleep 15
+  Write-Output $out
   $completed = Get-Job -State "Completed"
   ## Save logs to file and remove jobs
   If ($completed) {
@@ -42,8 +43,6 @@ Do {
       Write-Output "Saved output for job $name to $path\$name.log"
     }
   }
-  Write-Output $out
-  Start-Sleep 15
 } While (Get-Job -State "Running")
 
 ## Check if any tasks failed
@@ -59,11 +58,13 @@ Write-Output "All jobs finished"
 Write-Output "----"
 
 $expressionA = "docker manifest create $repository/$username/mrroboto:latest"
-$expressionB = "docker manifest create $repository/$username/mrroboto:latest"
+$expressionB = "docker manifest create $repository/$username/mrroboto_no-audio:latest"
 Foreach ($platform in $platforms){
-  $expressionA = "$expressionA $("--amend $repository/$username/mrroboto_no-audio:$platform")"
-  $expressionB = "$expressionB $("--amend $repository/$username/mrroboto_no-audio:$platform")"
+  $expressionA = "$expressionA $("--amend $repository/$username/mrroboto:$platform")"
+  $expressionB = "$expressionB $("--amend $repository/$username/mrroboto:$platform")"
 }
 
-Invoke-Expression $expression
+Invoke-Expression $expressionA
+Invoke-Expression $expressionB
 docker manifest push "$repository/$username/mrroboto:latest"
+docker manifest push "$repository/$username/mrroboto_no-audio:latest"

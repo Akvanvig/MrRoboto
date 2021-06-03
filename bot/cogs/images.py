98770f9c14c3bd535b4d64711e75757462b2b806
MrOctopus <shock.game@hotmail.com>
2021-06-03 22:28:05 +0200

    Spun out tesseract into executor

--- bot/cogs/images.py
+++ bot/cogs/images.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 from common import util_h
 from io import BytesIO
 from PIL import Image
+from functools import partial
 
 #
 # CLASSES
@@ -69,7 +70,9 @@ class Images(commands.Cog):
         images = await self._images_from_ctx(ctx)
 
         for i, image in enumerate(images, 1):
-            text = pytesseract.image_to_string(image)
+            image_partial = partial(pytesseract.image_to_string, image)
+
+            text = await self.client.loop.run_in_executor(None, image_partial)
             cleaned_text = util_h.remove_multi_newlines(text).strip()
 
             if not cleaned_text:
